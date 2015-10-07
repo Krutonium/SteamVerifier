e7abd17d2ff8062907ca291da483c8a0e78b815c
Kenton <pfckrutonium@gmail.com>
2015-10-07 05:28:57 -0400

    Inital Commit

---
+++ SteamVerifier/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.FolderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FIleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChooseSteamAppsFolderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ResultScreenshotFolderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChangeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BlackListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ResetToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ManageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.checkIfValidDone = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckingProgress = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'FolderBrowserDialog
+        '
+        Me.FolderBrowserDialog.Description = "Please select the folder where your games are installed - For example, ""C:\Progra" &
+    "m Files(x86)\Steam\steamapps\"""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FIleToolStripMenuItem, Me.BlackListToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(484, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FIleToolStripMenuItem
+        '
+        Me.FIleToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ChooseSteamAppsFolderToolStripMenuItem, Me.ResultScreenshotFolderToolStripMenuItem, Me.CloseToolStripMenuItem})
+        Me.FIleToolStripMenuItem.Name = "FIleToolStripMenuItem"
+        Me.FIleToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FIleToolStripMenuItem.Text = "FIle"
+        '
+        'ChooseSteamAppsFolderToolStripMenuItem
+        '
+        Me.ChooseSteamAppsFolderToolStripMenuItem.Name = "ChooseSteamAppsFolderToolStripMenuItem"
+        Me.ChooseSteamAppsFolderToolStripMenuItem.Size = New System.Drawing.Size(213, 22)
+        Me.ChooseSteamAppsFolderToolStripMenuItem.Text = "Choose SteamApps Folder"
+        '
+        'ResultScreenshotFolderToolStripMenuItem
+        '
+        Me.ResultScreenshotFolderToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.ChangeToolStripMenuItem})
+        Me.ResultScreenshotFolderToolStripMenuItem.Name = "ResultScreenshotFolderToolStripMenuItem"
+        Me.ResultScreenshotFolderToolStripMenuItem.Size = New System.Drawing.Size(213, 22)
+        Me.ResultScreenshotFolderToolStripMenuItem.Text = "Result Screenshot Folder"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'ChangeToolStripMenuItem
+        '
+        Me.ChangeToolStripMenuItem.Name = "ChangeToolStripMenuItem"
+        Me.ChangeToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.ChangeToolStripMenuItem.Text = "Change"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(213, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'BlackListToolStripMenuItem
+        '
+        Me.BlackListToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ResetToolStripMenuItem, Me.ManageToolStripMenuItem})
+        Me.BlackListToolStripMenuItem.Name = "BlackListToolStripMenuItem"
+        Me.BlackListToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.BlackListToolStripMenuItem.Text = "BlackList"
+        '
+        'ResetToolStripMenuItem
+        '
+        Me.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem"
+        Me.ResetToolStripMenuItem.Size = New System.Drawing.Size(102, 22)
+        Me.ResetToolStripMenuItem.Text = "Reset"
+        '
+        'ManageToolStripMenuItem
+        '
+        Me.ManageToolStripMenuItem.Name = "ManageToolStripMenuItem"
+        Me.ManageToolStripMenuItem.Size = New System.Drawing.Size(102, 22)
+        Me.ManageToolStripMenuItem.Text = "Add"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 38)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(460, 215)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(179, 259)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 30)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Start Verification"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'checkIfValidDone
+        '
+        Me.checkIfValidDone.Interval = 1000
+        '
+        'CheckingProgress
+        '
+        Me.CheckingProgress.Location = New System.Drawing.Point(12, 296)
+        Me.CheckingProgress.Name = "CheckingProgress"
+        Me.CheckingProgress.Size = New System.Drawing.Size(460, 23)
+        Me.CheckingProgress.TabIndex = 4
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.SteamVerifier.My.Resources.Resources.Donate
+        Me.PictureBox2.Location = New System.Drawing.Point(398, 264)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(74, 21)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 331)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.CheckingProgress)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Steam Game Verifier"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents FolderBrowserDialog As FolderBrowserDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FIleToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ChooseSteamAppsFolderToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents checkIfValidDone As Timer
+    Friend WithEvents ResultScreenshotFolderToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ChangeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BlackListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ResetToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ManageToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CheckingProgress As ProgressBar
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ SteamVerifier/Form1.vb
@@ -0,0 +1,226 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Runtime.InteropServices
+Imports System.Threading
+Public Class Form1
+    Private Declare Function SetForegroundWindow Lib "user32" _
+    (ByVal hWnd As IntPtr) As Boolean
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As _
+    Byte, ByVal bScan As Byte, ByVal dwFlags As Integer,
+    ByVal dwExtraInfo As Integer)
+    Private Const VK_SNAPSHOT As Short = &H2CS
+
+    Public Overloads Declare Function GetWindowRect Lib _
+    "User32" Alias "GetWindowRect" (ByVal hWnd As IntPtr,
+    ByRef lpRect As RECT) As Int32
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure RECT
+        Public Left As Integer
+        Public Top As Integer
+        Public Right As Integer
+        Public Bottom As Integer
+    End Structure
+
+
+    Dim Percent As String
+    Dim CurrentGame As String
+
+    Dim UserPath As String
+    Dim IsValidating As Boolean = False
+    Dim GameIDList As New List(Of String)
+    Dim Done As New List(Of String)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Icon = My.Resources.Steam
+        PictureBox1.Image = My.Resources.NotRunning
+        If Directory.Exists(My.Settings.GameDir64) = True Then
+            UserPath = My.Settings.GameDir64
+        Else
+            If Directory.Exists(My.Settings.GameDir32) = False Then
+                UserPath = My.Settings.GameDir32
+            Else
+                If Directory.Exists(My.Settings.GameDirOther) = False Then
+                    UserPath = My.Settings.GameDirOther
+                Else
+                    Call ChooseNewDir()
+                End If
+            End If
+        End If
+        For Each f In Directory.GetFiles(UserPath)
+            If f.EndsWith(".acf") Then
+                'GameIDList.Add(Path.GetFileNameWithoutExtension(f))
+                Dim GTemp As String = Path.GetFileNameWithoutExtension(f)
+                GTemp = GTemp.Replace("appmanifest_", "")
+                If My.Settings.BlackList.Contains(GTemp) = False Then
+                    GameIDList.Add(GTemp)
+                End If
+            End If
+        Next
+    End Sub
+    Private Sub ChooseNewDir()
+        FolderBrowserDialog.ShowNewFolderButton = False
+        FolderBrowserDialog.ShowDialog()
+        MsgBox(FolderBrowserDialog.SelectedPath)
+        My.Settings.GameDirOther = FolderBrowserDialog.SelectedPath
+        My.Settings.Save()
+        UserPath = My.Settings.GameDirOther
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        CheckProgressAndSaveImage(CurrentGame)
+        If IsValidating = False Then
+            Timer1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub CheckProgressAndSaveImage(ByVal Game As String)
+        For Each P In Process.GetProcesses
+            If P.MainWindowTitle.Trim = "" = False Then
+                If P.MainWindowTitle.ToUpper.Contains("VALIDATING") Then
+                    Dim Title = P.MainWindowTitle.Split("-"c)
+                    Dim Perc = Title(1).Split("%"c)
+                    Percent = Perc(0).Trim
+                    CheckingProgress.Value = Percent
+                    If Percent = "100" Then
+                        Dim bm As Bitmap = GetProgramImage(P.MainWindowHandle)
+                        If Directory.Exists(My.Settings.ScreenShotDir) = False Then
+                            Directory.CreateDirectory(My.Settings.ScreenShotDir)
+                        End If
+                        bm.Save(My.Settings.ScreenShotDir & Game & ".png", System.Drawing.Imaging.ImageFormat.Png)
+                        P.CloseMainWindow()
+                        Thread.Sleep(2000)
+                        IsValidating = False
+                    End If
+                End If
+            End If
+        Next
+    End Sub
+    Private Function GetProgramImage(ByVal handle As IntPtr) As Image
+        ' Bring it to the top.
+        SetForegroundWindow(handle)
+
+        ' Get the desktop image.
+        Dim desktop_bm As Bitmap = GetDesktopImage()
+
+        ' Find the program's window.
+        Dim program_rect As RECT
+        GetWindowRect(handle, program_rect)
+
+        ' Copy that part of the desktop image.
+        Dim wid As Integer = program_rect.Right -
+            program_rect.Left
+        Dim hgt As Integer = program_rect.Bottom -
+            program_rect.Top
+        Dim dest_rect As New Rectangle(0, 0, wid, hgt)
+        Dim src_rect As New Rectangle(program_rect.Left,
+            program_rect.Top, wid, hgt)
+        Dim new_bm As New Bitmap(wid, hgt)
+        Using gr As Graphics = Graphics.FromImage(new_bm)
+            gr.DrawImage(desktop_bm, dest_rect, src_rect,
+                GraphicsUnit.Pixel)
+        End Using
+
+        Return new_bm
+    End Function
+    Private Function GetDesktopImage() As Image
+        keybd_event(System.Windows.Forms.Keys.Snapshot, 0, 0, 0)
+        System.Threading.Thread.Sleep(200)
+
+        If Clipboard.ContainsImage() Then Return _
+            Clipboard.GetImage()
+        Return Nothing
+    End Function
+    Private Sub cmdChangeSteamAppsFolder_Click(sender As Object, e As EventArgs)
+        ChooseNewDir()
+    End Sub
+    Private Sub ChooseSteamAppsFolderToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ChooseSteamAppsFolderToolStripMenuItem.Click
+        ChooseNewDir()
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Sub UpdateImage(ByVal GameID As Integer)
+        Try
+            Dim ImageURL As String = "http://cdn.akamai.steamstatic.com/steam/apps/" & GameID & "/header.jpg"
+            Dim WS As New WebClient
+            Dim byteImage As Byte() = WS.DownloadData(ImageURL) 'this should contain your data
+            Dim myimage As Image
+            Dim ms As System.IO.MemoryStream = New System.IO.MemoryStream(byteImage)
+            myimage = System.Drawing.Image.FromStream(ms)
+            PictureBox1.Image = myimage
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub DbToolStripMenuItem1_Click(sender As Object, e As EventArgs)
+        For Each item In GameIDList
+            UpdateImage(item)
+            MsgBox("Waiting")
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        IsValidating = False
+        checkIfValidDone.Enabled = True
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub checkIfValidDone_Tick(sender As Object, e As EventArgs) Handles checkIfValidDone.Tick
+        If IsValidating = False Then
+            For Each item In GameIDList
+                If Done.Contains(item) = False Then
+                    IsValidating = True
+                    Process.Start("steam://validate/" & item)
+                    CurrentGame = item
+                    Done.Add(item)
+                    UpdateImage(item)
+                    Timer1.Enabled = True
+                    Exit For
+                End If
+            Next
+        End If
+        If IsValidating = False Then
+            Button1.Enabled = True
+            MsgBox("Verification of your games is complete!", MsgBoxStyle.OkOnly, "Verified!")
+        End If
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Process.Start(My.Settings.ScreenShotDir)
+    End Sub
+
+    Private Sub ChangeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ChangeToolStripMenuItem.Click
+        Dim J As New FolderBrowserDialog
+        J.Description = "Please choose where you want the result shots to go:"
+        J.ShowNewFolderButton = True
+        J.ShowDialog()
+        If J.SelectedPath = "" = False Then
+            My.Settings.ScreenShotDir = J.SelectedPath
+        End If
+    End Sub
+
+    Private Sub ResetToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ResetToolStripMenuItem.Click
+        Dim P = MsgBox("Are you sure?", MsgBoxStyle.YesNo)
+        If P = MsgBoxResult.Yes Then
+            My.Settings.BlackList = Nothing
+            My.Settings.BlackList.Add(" ")
+        End If
+    End Sub
+
+    Private Sub ManageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ManageToolStripMenuItem.Click
+
+        My.Settings.BlackList.Add(InputBox("Please enter the Game ID to blacklist.", "Blacklist"))
+        Dim TempList As New List(Of String)
+        For Each item In GameIDList
+            If My.Settings.BlackList.Contains(item) = False Then
+                TempList.Add(item)
+            End If
+        Next
+        GameIDList = TempList
+        TempList = Nothing
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ELBKFBFBRBT46")
+    End Sub
+End Class
